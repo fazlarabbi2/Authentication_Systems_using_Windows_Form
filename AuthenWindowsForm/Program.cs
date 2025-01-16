466da9459c97a25006564cdf18e1f9b9ee2cfa3b
fazlarabbi2 <rabbifazla4@gmail.com>
2025-01-16 22:27:21 +0600

    Change startup form from Dashboard to frmLogin
    The application startup form has been changed from `Dashboard` to `frmLogin`. This means that when the application is launched, it will now display the login form (`frmLogin`) instead of the dashboard form (`Dashboard`).

--- AuthenWindowsForm/Program.cs
+++ AuthenWindowsForm/Program.cs
@@ -13,7 +13,7 @@ namespace AuthenWindowsForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Dashboard());
+            Application.Run(new frmLogin());
         }
     }
 }
